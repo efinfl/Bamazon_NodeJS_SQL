0eaab39a078e1995f777b7c0aa62b0c52a54bcdf
EF <ef@EFs-MacBook-Pro.local>
2018-12-14 18:28:32 -0500

    added README file

---
+++ README.md
@@ -0,0 +1,22 @@
+# Bamazon NodeJS SQL
+This is a command line node JS and SQL app in which a user can "buy" items from a hypothetical store, "Bamazon".
+
+## How it works
+
+### Product Listing 
+A table of available items is displayed. Each item displays it's item id, product name, department, price, and stock quantity.
+
+### Product Id Prompt
+The user is asked to input the id number of the product desired. If the user's input is invalid (such as a letter), they are asked to resubmit.
+
+### Quantity Prompt
+Once a valid item id is input, the user is asked for the quantity desired. If the quantity exceeds the stock quantity, they are asked to submit a lower number.
+
+### Final "Purchase" and Stock Quantity Update
+Once a valid quantity has been input, the user is notified that the order has been submitted. The quantity is updated, and new table is displayed.
+
+#### Demo
+<img src="https://github.com/efinfl/Bamazon_NodeJS_SQL/blob/master/demo-bamazon.gif">
+You may also click link for original Screencast of this gif: 
+
+https://drive.google.com/file/d/1K70CFLkY0YT1xMKWXqo77Wa03FqQGV1S/view?usp=sharing
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -16,7 +16,7 @@ var connection = mysql.createConnection({
 var answerId;
 var quantPass;
 
-// Connects to the database using connection variable above
+// ======= Connects to the database using connection variable above =========
 connection.connect(function (err) {
     if (err) throw err;
     
@@ -26,7 +26,7 @@ connection.connect(function (err) {
 });
 
 
-// This gets all the items and displays it in a table
+// ============= This gets all the items and displays it in a table ===========
 function getItems() {
     
     //Selects all from the table named products
@@ -45,8 +45,9 @@ function getItems() {
 
 
 
-/* Asks for the id of the item id of the product desired using inquirer.
-Results come from the getItems function*/
+/* ========= Asks for the id of the item id of the product desired using inquirer.
+Results come from the getItems function ====================== */
+
 function promptId(results) {
     inquirer
         .prompt({
@@ -74,9 +75,10 @@ function promptId(results) {
                 console.log("You've chosen product ID: " + chalk.bold.yellow(answerId));
                 console.log("--------------------------------------")
                 
-                // 
+                // Fires promptQuantity function 
                 promptQuantity();
             }
+            // Other let them know not valid and refire promptId
             else {
                 console.log("Not a valid id. Try again");
                 promptId(results);
@@ -85,53 +87,68 @@ function promptId(results) {
 
         });
 }
-
+// ========================== Asks for quantity =======================
 function promptQuantity() {
     inquirer
         .prompt({
             name: "quantity",
             type: "input",
             message: "How many would you like",
+            
+            // validates that the answer is a number
             validate: function (value) {
-                quantPass = value; // validates that the answer is a number
+                quantPass = value; 
                 if (quantPass > 0) {
-                    // console.log("This is pass " + quantPass)
+                
                     return true;
                 }
                 else {
                     return "This is not a number. Try again";
                 }
             }
+        
         }).then(function (answer) {
+            
+            // quantPass is a string, so it's then turned into an int
             quantPass = parseInt(quantPass);
+            
+            // This selects all the rows from the table ("products") that has the id of the answerID
             connection.query("SELECT * FROM products WHERE item_id = " + answerId,
                 function (err, results) {
-                    // console.log("Quantity in stock: " + results[0].stock_quantity)
-                    // console.log("Quantity chosen: " + quantPass)
+                    
+                    /* The results are therefor the row that corresponds to that id,
+                    and we assign a variable to the stock_quantity value it contains*/
                     var quantInStock = results[0].stock_quantity
+                    
                     if (err) {
                         throw err;
                     }
-
+                    /* ----If there are no errors and quantPass (amount requested) is 
+                    less than or equal to the following happens-----*/
+                    
+                    // Checks to make sure there is enough in stock
                     else if (!err && quantPass <= quantInStock) {
-                          // Get total cost
-                        // Get price of product
+                        
+                        // -----------Get total cost------------
+                        // Gets price of product and puts into a variable
                         let price = results[0].price
-                        // Multiply by quantPass
+                        
+                        // Gets quantity chosen and multiplies by cost for total cost
                         let totalCost = quantPass * price;
-                        // console.log("--------------------------------------");
+                        
+                        // Display the order confirmation an total cost);
                         console.log("You want " + chalk.bold.yellow(quantPass) + " of item " + chalk.bold.yellow(answerId));
                         console.log(chalk.bold.green("Your order has been placed."));
                         console.log(chalk.bold.green("Your total cost is: " + "$" + totalCost));
                         console.log("--------------------------------------");
+                        
+                        // updateTable is fired
                         updateTable(answerId)
                         
-                      
-                        // TODO Move this
-                        // connection.end();
 
                     }
-
+                    /* If there isn't enough in stock, then notification is given and 
+                    promptQuantity is refired*/
                     else if (!err && quantPass > quantInStock) {
                         console.log(chalk.bold.red("Not enough in stock. Choose a lower number."));
                         console.log("--------------------------------------");
@@ -145,13 +162,14 @@ function promptQuantity() {
         });
 
 }
-
-// Make function to connect database, then SELECT BY item_id UPDATE stock_quantity and do the math.
-
+// =========================== Table is updated by... =============================
 function updateTable(answerId) {
+    
+    /* subtracting stock_quantity from quantPass(?) where the item_id is answerId(?) */
     connection.query('UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?', [quantPass, answerId], function (err, res) {
         if (err) throw err;
         
+        // Displays the the table all over again with its update.
         getItems()
     })
 };
