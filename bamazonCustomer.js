4a0e62184dcfc58d46d905f2247268ad8a46d67d
EF <ef@EFs-MacBook-Pro.local>
2018-12-14 15:45:49 -0500

    all is functional

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -19,33 +19,42 @@ var quantPass;
 // Connects to the database using connection variable above
 connection.connect(function (err) {
     if (err) throw err;
-    // console.log("connect as id: " + connection.threadId);
-    getItems() // getItem function is fired
+    
+    // getItem function is fired
+    getItems() 
 
 });
 
 
 // This gets all the items and displays it in a table
 function getItems() {
-    connection.query("SELECT * FROM products", //Selects all from the table named products
+    
+    //Selects all from the table named products
+    connection.query("SELECT * FROM products", 
         function (err, results) {
             if (err) throw err;
-            console.table(results); //logs formatted table
-            promptId(results); // fires promptUser function
-            // connection.end() // closes connection
+            
+            //logs formatted table
+            console.table(results); 
+            
+            // fires promptUser function
+            promptId(results); 
 
         });
 }
 
 
 
-// Displays question and receives answer
+/* Asks for the id of the item id of the product desired using inquirer.
+Results come from the getItems function*/
 function promptId(results) {
     inquirer
         .prompt({
             name: "item_id",
             type: "input",
             message: "What's the ID of the product you want?",
+            
+            /* Validates that the input is actually a number */
             validate: function (value) {
                 answerId = value
                 if (answerId > 0) {
@@ -56,17 +65,21 @@ function promptId(results) {
                 }
             }
         }).then(function (answers) {
-            answerId = answers.item_id; // Takes answer and keeps it as variable
-            if (answerId < (results.length + 1) && answerId > 0) { // If less than the result's index and greater than 0, console.log the id
-
-                // console.log("--------------------------------------")
+            
+            // Reassigning answerID (the id chosen) to the answers (answer {item_id: "3"})
+            answerId = answers.item_id;
+            
+            // If less than the result's index and greater than 0, console.log the id
+            if (answerId < (results.length + 1) && answerId > 0) { 
                 console.log("You've chosen product ID: " + chalk.bold.yellow(answerId));
                 console.log("--------------------------------------")
-
+                
+                // 
                 promptQuantity();
             }
             else {
                 console.log("Not a valid id. Try again");
+                promptId(results);
                 
             }
 
@@ -100,12 +113,22 @@ function promptQuantity() {
                         throw err;
                     }
 
-                    else if (!err && quantPass < quantInStock) {
+                    else if (!err && quantPass <= quantInStock) {
+                          // Get total cost
+                        // Get price of product
+                        let price = results[0].price
+                        // Multiply by quantPass
+                        let totalCost = quantPass * price;
                         // console.log("--------------------------------------");
                         console.log("You want " + chalk.bold.yellow(quantPass) + " of item " + chalk.bold.yellow(answerId));
                         console.log(chalk.bold.green("Your order has been placed."));
+                        console.log(chalk.bold.green("Your total cost is: " + "$" + totalCost));
                         console.log("--------------------------------------");
-                        connection.end()
+                        updateTable(answerId)
+                        
+                      
+                        // TODO Move this
+                        // connection.end();
 
                     }
 
@@ -125,6 +148,13 @@ function promptQuantity() {
 
 // Make function to connect database, then SELECT BY item_id UPDATE stock_quantity and do the math.
 
+function updateTable(answerId) {
+    connection.query('UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?', [quantPass, answerId], function (err, res) {
+        if (err) throw err;
+        
+        getItems()
+    })
+};
 
 
 
